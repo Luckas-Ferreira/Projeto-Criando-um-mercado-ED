784a7e712d8e6b0fb1a3151c3f3fa3473f839d0f
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-06-26 11:59:18 -0300

    alterações finais nos nomes

--- TabelaHash.py
+++ TabelaHash.py
@@ -19,20 +19,20 @@ class  TabelaHash:
         self.slots = [None] * self.size
         self.data = [None] * self.size
 
-    def adcionar(self,key,data):
-      valorHash = self.funcaoHash(key,len(self.slots))
+    def put(self,key,data):
+      hashvalue = self.hashfunction(key,len(self.slots))
 
-      if self.slots[valorHash] == None:
-        self.slots[valorHash] = key
-        self.data[valorHash] = data
+      if self.slots[hashvalue] == None:
+        self.slots[hashvalue] = key
+        self.data[hashvalue] = data
       else:
-        if self.slots[valorHash] == key:
-          self.data[valorHash] = data  #replace
+        if self.slots[hashvalue] == key:
+          self.data[hashvalue] = data  #replace
         else:
-          nextslot = self.refazer(valorHash,len(self.slots))
+          nextslot = self.rehash(hashvalue,len(self.slots))
           while self.slots[nextslot] != None and \
                           self.slots[nextslot] != key:
-            nextslot = self.refazer(nextslot,len(self.slots))
+            nextslot = self.rehash(nextslot,len(self.slots))
 
           if self.slots[nextslot] == None:
             self.slots[nextslot]=key
@@ -40,14 +40,14 @@ class  TabelaHash:
           else:
             self.data[nextslot] = data #replace
 
-    def funcaoHash(self,key,size):
+    def hashfunction(self,key,size):
          return key%size
 
-    def refazer(self,oldhash,size):
+    def rehash(self,oldhash,size):
         return (oldhash+1)%size
 
     def get(self,key):
-      startslot = self.funcaoHash(key,len(self.slots))
+      startslot = self.hashfunction(key,len(self.slots))
 
       data = None
       stop = False
@@ -59,7 +59,7 @@ class  TabelaHash:
            found = True
            data = self.data[position]
          else:
-           position=self.refazer(position,len(self.slots))
+           position=self.rehash(position,len(self.slots))
            if position == startslot:
                stop = True
       return data
@@ -68,7 +68,7 @@ class  TabelaHash:
         return self.get(key)
 
     def __setitem__(self,key,data):
-        self.adcionar(key,data)
+        self.put(key,data)
 
 
 Tabela = TabelaHash()
